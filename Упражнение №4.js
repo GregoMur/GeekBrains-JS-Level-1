9ac8840bbc8b34633db75ad35c966a874ffedcc2
[GregoMur] <[bekir.murtazaev@gmail.com]>
2021-02-27 15:11:29 +0300

    Упражнения второго урока

---
+++ Упражнение №4.js
@@ -0,0 +1,75 @@
+var a = Math.round(Math.random() * (15 - 0) + 0);
+
+console.log('Способ с помощью switch');
+
+// Оператор switch
+switch(a){
+	case 0:
+	console.log(0);
+
+	case 1:
+	console.log(1);
+
+	case 2:
+	console.log(2);
+
+	case 3:
+	console.log(3);
+
+	case 4:
+	console.log(4);
+
+	case 5:
+	console.log(5);
+
+	case 6:
+	console.log(6);
+
+	case 7:
+	console.log(7);
+
+	case 8:
+	console.log(8);
+
+	case 9:
+	console.log(9);
+
+	case 10:
+	console.log(10);
+
+	case 11:
+	console.log(11);
+
+	case 12:
+	console.log(12);
+
+	case 13:
+	console.log(13);
+
+	case 14:
+	console.log(14);
+
+	case 15:
+	console.log(15);
+	break;
+
+	default:
+	console.log('Значение не входит в числовой диапазон');
+	break;
+}
+
+console.log('Способ с помощью рекурсии');
+
+// *Использование рекурсии
+function showNumber(a) {
+	if(a < 0) {
+		return console.log('Значение не входит в числовой диапазон');
+	}
+	if (a <= 15) {
+		console.log(a);
+		showNumber(a + 1);
+	}
+	
+} 
+
+showNumber(a);
