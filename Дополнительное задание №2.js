0fb90363c04cfe1a06d7e1781d4d521670db8c8d
[GregoMur] <[bekir.murtazaev@gmail.com]>
2021-03-01 10:36:25 +0300

    Упражнения третьего урока

---
+++ Дополнительное задание №2.js
@@ -0,0 +1,11 @@
+// Нарисовать пирамиду с помощью console.log
+for (var i = 1; i <= 20; i++) {
+		if (i > 1) {
+			console.log('\n');
+		}
+	for (var j = 0; j < i; j++) {
+		console.log('x');
+		
+	}
+	
+}
